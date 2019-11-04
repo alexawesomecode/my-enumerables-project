b0491f296a14dd45e5fe7b15bccaace6f8d2a992
alex <alexawesomecode@gmail.com>
2019-11-04 16:30:59 -0300

    refactored some methods

--- enumerables.rb
+++ enumerables.rb
@@ -1,121 +1,138 @@
 module Enumerable
-    def my_each
-        if block_given?
-            item = 0
-            while item < self.size
-                yield(self[item])
-             item+=1
-            end
-        end
-    
+  def my_each
+    return to_enum(:my_each) unless block_given?
+    if block_given?
+      item = 0
+      while item < size
+        yield(self[item])
+        item += 1
+      end
+    end
+
     self
+  end
+
+  def my_each_with_index
+    return to_enum(:my_each_with_index) unless block_given?
+    if block_given?
+      index = 0
+      while index < size
+        yield(self[index], index)
+        index += 1
+      end
+    end
+    self
+  end
+
+  def my_select
+    return to_enum(:my_select) unless block_given?
+    if block_given?
+      filtered = []
+      my_each { |x| filtered.push(x) if yield(x) }
+    end
+    filtered
+  end
+
+  def my_all?(pattern = nil)
+    not_all = 0
+    if pattern.class == Regexp
+      my_each { |x| not_all = 1 if pattern.match(x).nil? }
+      not_all != 1
+    end
+    if block_given?
+      index = 0
+      while index < size
+        not_all = 1 unless yield(self[index])
+        index += 1
+        break if not_all == 1
+      end
+
+    end
+    not_all != 1
+  end
+
+  def my_any?(pattern = nil, &block)
+    any = 0
+    if pattern.class == Regexp
+      my_each { |x| any = 1 unless pattern.match(x).nil? }
+      any == 1
+    end
+    if block_given?
+      any = 1 unless my_none?(pattern = nil, &block)
+      any == 1
+    end
+  end
+
+  def my_none?(pattern = nil)
+    some = 0
+    if pattern.class == Regexp
+      my_each { |x| some = 1 unless pattern.match(x).nil? }
+      some == 1
+    end
+    if block_given?
+      index = 0
+      while index < size
+        some = 1 if yield(self[index])
+        index += 1
+        break if some == 1
+      end
+    end
+    some != 1
+  end
+
+  def my_count(j = nil)
+    count = 0
+    return to_enum(:my_count) if j.nil? && !block_given?
+    if block_given?
+      index = 0
+      while index < size
+        count += 1 if yield(self[index])
+        index += 1
+      end
     end
 
+    my_each { |x| count += 1 if x == j } unless j.nil?
+    count
+  end
 
+  def my_map(proc = nil)
+    return to_enum(:my_map) unless block_given?
+    new_array = []
+    my_each { |x| new_array << proc.call(x) } if proc
+    my_each { |x| new_array << yield(x) } if block_given?
+    new_array
+  end
 
-    def my_each_with_index
-        if block_given?
-            index = 0
-            while index < self.size
-                yield(self[index], index)
-                index+=1
-            end
-        end
-        self
+  def my_inject(args = nil)
+    if args.class == Symbol
+    end
+    if args.class == Integer
+      acumulator = args[0]
+      index = 1
+      while index < size
+        acumulator = yield(acumulator, self[index])
+        index += 1
+      end
+      acumulator
+
     end
 
-    def my_select
-        if block_given?
-            filtered = []
-            index = 0
-            self.my_each { |x| filtered.push(x) if yield(x) }
-        end
-        filtered
-    end
-    
-    def my_all?
-        if block_given?
-            not_all = 0
-            index = 0
-            while index < self.size
-                not_all = 1 if !yield(self[index])
-                index+=1
-                break if not_all == 1
-            end
-            
-        end
-        not_all == 1 ? false : true
-    end
+    if block_given?
+      acumulator = self[0]
+      index = 1
+      while index < size
+        acumulator = yield(acumulator, self[index])
+        index += 1
+      end
 
-    def my_any?
-        if block_given?
-            any = 0
-            index = 0
-            while index < self.size
-                any = 1 if yield(self[index])
-                index+=1
-                break if any == 1
-            end
-        end
-        any == 1 ? true : false
     end
-
-    def my_none?
-        if block_given?
-            some = 0
-            index = 0
-            while index < self.size
-                some = 1 if yield(self[index])
-                index+=1
-                break if some == 1
-            end
-        end
-        some == 1 ? false : true
-    end
-
-    def my_count
-        if block_given?
-            count = 0
-            index = 0
-            while index < self.size
-                count+=1 if yield(self[index])
-                index+=1
-            end
-        end
-        count
-    end
-
-    def my_map(proc = nil)
-        new_array = []
-        index = 0
-        if proc 
-            self.my_each { |x| new_array << proc.call(x)}
-        end
-        if block_given?
-            self.my_each {|x| new_array << yield(x)}
-        end             
-        new_array
-    end
-
-    def my_inject
-        if block_given?
-            acumulator = self[0]
-            index = 1
-            while index < self.size
-                acumulator = yield(acumulator, self[index])
-                index+=1
-            end
-
-        end
-        acumulator
-    end
-
-    def multiply_els
-
-        self.my_inject{|a,b| a * b}
-         
-    end
+    acumulator
+  end
 end
 
+def multiply_els(x)
+  x.my_inject([x]) { |a, b| a * b }
+end
 
-# puts [12,3,4,4,33].my_map {|x| x ** 2}
+puts [1, 2, 3, 3, 3, 5].my_any? { |x| x == 1 }
+
